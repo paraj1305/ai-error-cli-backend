24963897638d64ced32c2478371afcf1433d84e4
nikunjd11 <nikunj@dstrict11.com>
2025-12-26 17:24:55 +0530

    Initial FastAPI project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+#run fast app with: uvicorn main:app --reload
+
+from fastapi import FastAPI
+from backend.routes.error_explainer import router as error_router
+
+app = FastAPI(
+    title="AI Error Explainer API",
+    version="1.0.0"
+)
+
+app.include_router(error_router)
